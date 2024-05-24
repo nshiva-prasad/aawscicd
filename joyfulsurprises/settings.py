123d8810651bae55e983e15f37f04d4994c4dd60
nshiva-prasad <nspr12345@gmail.com>
2024-05-24 15:05:14 +0530

    updated the allowed host and cors

--- joyfulsurprises/settings.py
+++ joyfulsurprises/settings.py
@@ -28,11 +28,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0:8000', '18.61.224.88']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0:8000', '18.61.5.9']
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'http://18.61.224.88','https://18.61.224.88']
+CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1','https://18.61.5.9']
 
 # Application definition
 
